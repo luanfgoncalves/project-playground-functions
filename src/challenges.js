3964d1d5d8841e574e49e61f1eef98b90a656df1
Luan Filipe Gonçalves <luanfgoncalves@pop-os.localdomain>
2022-04-18 13:03:07 -0300

    correção de funções

--- src/challenges.js
+++ src/challenges.js
@@ -18,9 +18,9 @@ function splitSentence(splitSentenceParam) {
   return splitSentenceAnswer;
 }
 
-// Desafio 4
+// Desafio 4 - erro
 function concatName(concatNameParam) {
-  return concatNameParam[concatNameParam.length] + ', ' + concatNameParam[0] + '.';
+  return concatNameParam[concatNameParam.length-1] + ', ' + concatNameParam[0];
 }
 
 // Desafio 5
@@ -28,9 +28,9 @@ function footballPoints(wins, ties) {
   return (wins * 3) + ties;
 }
 
-// Desafio 6
+// Desafio 6 - corrigir
 function highestCount(highestCountParam) {
-  let maxValue = 0;
+  let maxValue = highestCountParam[0];
   let maxValueOcurrence = 0;
   for (let index = 0; index < highestCountParam.length; index += 1) {
     if (highestCountParam[index] > maxValue) {
@@ -69,9 +69,9 @@ function catAndMouse(mouse, cat1, cat2) {
   } else {
     cat2Distance = mouse - cat2;
   }
-  if (cat1Distance > cat2Distance) {
+  if (cat1Distance < cat2Distance) {
     catAndMouseAnswer = 'cat1';
-  } else if (cat2Distance > cat1Distance) {
+  } else if (cat2Distance < cat1Distance) {
     catAndMouseAnswer = 'cat2';
   } else {
     catAndMouseAnswer = 'os gatos trombam e o rato foge';
@@ -79,7 +79,7 @@ function catAndMouse(mouse, cat1, cat2) {
   return catAndMouseAnswer;
 }
 
-// Desafio 8
+// Desafio 8 - corrigir
 function fizzBuzz(fizzBuzzParam) {
   let fizzBuzzAnswer = [];
   for (let index = 0; index < fizzBuzzParam.length; index += 1) {
@@ -91,7 +91,7 @@ function fizzBuzz(fizzBuzzParam) {
     }
     if (fizzBuzzParam[index] % 5 === 0 && fizzBuzzParam[index] % 3 === 0) {
       fizzBuzzAnswer.push('fizzBuzz');
-    } else {
+    } else if (fizzBuzzParam[index] % 5 !== 0 && fizzBuzzParam[index] % 3 !== 0){
       fizzBuzzAnswer.push('bug!');
     }
   }
