3981ea469182877e90f7b8349fe1eaabae2e9a8c
Luan Filipe Gonçalves <luanfgoncalves@pop-os.localdomain>
2022-04-18 12:06:04 -0300

    coloca os códigos nos arquivos corretos

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,42 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(generatePhoneNumberParam) {
+  let generatePhoneNumberAnswer = '(' + generatePhoneNumberParam[0] + generatePhoneNumberParam[1] + ') ';
+  for (let index = 2; index < generatePhoneNumberParam.length - 4; index += 1) {
+    generatePhoneNumberAnswer += (generatePhoneNumberParam[index]);
+  }
+  generatePhoneNumberAnswer += '-';
+  for (let index = 7; index < generatePhoneNumberParam.length; index += 1) {
+    generatePhoneNumberAnswer += (generatePhoneNumberParam[index]);
+  }
+  if (generatePhoneNumberParam.length !== 11) {
+    generatePhoneNumberAnswer = 'Array com tamanho incorreto';
+  }
+  for (let index = 0; index < generatePhoneNumberParam.length; index += 1) {
+    if (generatePhoneNumberParam[index] < 0 || generatePhoneNumberParam[index] > 9) {
+      generatePhoneNumberAnswer = 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return generatePhoneNumberAnswer;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let triangleVialiliy = false;
+  if (lineA < 0) {
+    lineA *= -1;
+  }
+  if (lineB < 0) {
+    lineB *= -1;
+  }
+  if (lineC < 0) {
+    lineC *= -1;
+  }
+  if ((lineB - lineC) < lineA < (lineB + lineC) || (lineA - lineC) < lineB < (lineA + lineC) || (lineA - lineB) < lineC < (lineA + lineB)) {
+    triangleVialiliy = true;
+  } else {
+    triangleVialiliy = false;
+  }
+  return triangleVialiliy;
 }
 
 // Desafio 13
